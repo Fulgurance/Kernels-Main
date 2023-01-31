548d72624a71dfd917500367abc5eff815b46249
fulgurance <zohran.londais@gmail.com>
2023-01-31 21:18:17 +0000

    Patched missing dir for Linux Kernel

--- Linux/5.13.12/5.13.12.cr
+++ Linux/5.13.12/5.13.12.cr
@@ -3,8 +3,9 @@ class Target < ISM::Software
     def prepareInstallation
         super
         makeDirectory("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/src/")
+        makeDirectory("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/share/doc/")
         copyDirectory("#{mainWorkDirectoryPath(false)}Documentation","#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/share/doc/linux-5.13.12")
-        moveFile("#{workDirectoryPath(false)}linux-5.13.12","#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/src/")
+        moveFile("#{workDirectoryPath(false)}/linux-5.13.12","#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/src/")
     end
 
 end

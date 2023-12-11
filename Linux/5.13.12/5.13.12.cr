86ca8c42332d9aa413f8604ce9779cf13ce525ca
Fulgurance <zohran.londais@gmail.com>
2023-12-11 17:32:32 +0000

    Temporary patch

--- Linux/5.13.12/5.13.12.cr
+++ Linux/5.13.12/5.13.12.cr
@@ -1,12 +1,35 @@
 class Target < ISM::Software
+
+    def configure
+        super
+
+        if option("Pass1")
+            makeSource(["mrproper"],mainWorkDirectoryPath)
+        end
+    end
+
+    def build
+        super
+
+        if option("Pass1")
+            makeSource(["headers"],mainWorkDirectoryPath)
+        end
+    end
     
     def prepareInstallation
         super
 
-        makeDirectory("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/src/")
-        makeDirectory("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/share/doc/")
-        copyDirectory("#{mainWorkDirectoryPath(false)}Documentation","#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/share/doc/linux-5.13.12")
-        moveFile("#{workDirectoryPath(false)}/Sources","#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/src/linux-5.13.12")
+        if option("Pass1")
+            deleteAllHiddenFilesRecursively("#{mainWorkDirectoryPath}usr/include")
+            deleteFile("#{mainWorkDirectoryPath}usr/include/Makefile")
+            makeDirectory("#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}/usr")
+            copyDirectory("#{mainWorkDirectoryPath}usr/include", "#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}/usr")
+        else
+            makeDirectory("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/src/")
+            makeDirectory("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/share/doc/")
+            copyDirectory("#{mainWorkDirectoryPath(false)}Documentation","#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/share/doc/linux-5.13.12")
+            moveFile("#{workDirectoryPath(false)}/Sources","#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/src/linux-5.13.12")
+        end
     end
 
 end
--- Linux/6.5.1/6.5.1.cr
+++ Linux/6.5.1/6.5.1.cr
@@ -1,12 +1,35 @@
 class Target < ISM::Software
     
+    def configure
+        super
+
+        if option("Pass1")
+            makeSource(["mrproper"],mainWorkDirectoryPath)
+        end
+    end
+
+    def build
+        super
+
+        if option("Pass1")
+            makeSource(["headers"],mainWorkDirectoryPath)
+        end
+    end
+
     def prepareInstallation
         super
 
-        makeDirectory("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/src/")
-        makeDirectory("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/share/doc/")
-        copyDirectory("#{mainWorkDirectoryPath(false)}Documentation","#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/share/doc/linux-6.5.1")
-        moveFile("#{workDirectoryPath(false)}/Sources","#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/src/linux-6.5.1")
+        if option("Pass1")
+            deleteAllHiddenFilesRecursively("#{mainWorkDirectoryPath}usr/include")
+            deleteFile("#{mainWorkDirectoryPath}usr/include/Makefile")
+            makeDirectory("#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}/usr")
+            copyDirectory("#{mainWorkDirectoryPath}usr/include", "#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}/usr")
+        else
+            makeDirectory("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/src/")
+            makeDirectory("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/share/doc/")
+            copyDirectory("#{mainWorkDirectoryPath(false)}Documentation","#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/share/doc/linux-6.5.1")
+            moveFile("#{workDirectoryPath(false)}/Sources","#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/src/linux-6.5.1")
+        end
     end
 
 end

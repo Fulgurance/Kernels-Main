781cfb294025fbd2233ae183b48b3c0b669faa17
Fulgurance <zohran.londais@gmail.com>
2024-05-19 09:51:25 +0100

    Added new selinux kernel version

---
+++ Selinux-Kernel/6.9.0/6.9.0.cr
@@ -0,0 +1,41 @@
+class Target < ISM::Software
+    
+    def configure
+        super
+
+        if option("Pass1")
+            makeSource(["mrproper"],mainWorkDirectoryPath)
+        end
+    end
+
+    def build
+        super
+
+        if option("Pass1")
+            makeSource(["headers"],mainWorkDirectoryPath)
+        end
+    end
+
+    def prepareInstallation
+        super
+
+        if option("Pass1")
+            deleteAllHiddenFilesRecursively("#{mainWorkDirectoryPath}usr/include")
+            deleteFile("#{mainWorkDirectoryPath}usr/include/Makefile")
+            makeDirectory("#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}/usr")
+            copyDirectory("#{mainWorkDirectoryPath}usr/include", "#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}/usr")
+        else
+            makeDirectory("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/src/")
+            makeDirectory("#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/share/doc/")
+            copyDirectory("#{mainWorkDirectoryPath(false)}Documentation","#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/share/doc/sellinux-kernel-6.9.0")
+            moveFile("#{workDirectoryPath(false)}/Sources","#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}usr/src/selinux-kernel-6.9.0")
+        end
+    end
+
+    def install
+        super
+
+        updateKernelOptionsDatabase
+    end
+
+end
